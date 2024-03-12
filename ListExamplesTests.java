d10a006c6966dd733bd1b22621879c536dc90c0c
kdsparks <156236500+kdsparks@users.noreply.github.com>
2024-03-11 23:08:59 -0700

    Update ListExamplesTests.java

--- ListExamplesTests.java
+++ ListExamplesTests.java
@@ -6,19 +6,11 @@ import java.util.ArrayList;
 
 public class ListExamplesTests {
 	@Test(timeout = 500)
-	public void testMerge1() {
+	public void testMerge() {
     		List<String> l1 = new ArrayList<String>(Arrays.asList("x", "y"));
 		List<String> l2 = new ArrayList<String>(Arrays.asList("a", "b"));
 		List<String> mergedList = new ArrayList<String>();
-		assertArrayEquals(new String[]{ "a", "b", "x", "y"}, ListExamples.merge(l1, l2, mergedList).toArray());
-	}
-
-	public void testMerge2() {
-    		List<String> l1 = new ArrayList<String>(Arrays.asList("x", "y"));
-		List<String> l2 = new ArrayList<String>(Arrays.asList("a", "b"));
-		List<String> l3 = new ArrayList<String>(Arrays.asList("c", "d", "e"));
-		List<String> mergedList = new ArrayList<String>();
-		assertArrayEquals(new String[]{ "a", "b", "x", "y"}, ListExamples.merge(l1, l2, mergedList).toArray());
-		assertArrayEquals(new String[]{ "a", "b", "c", "d", "e"}, ListExamples.merge(l2, l3, mergedList).toArray());
+		ListExamples.merge(l1, l2, mergedList);
+		assertArrayEquals(new String[]{ "a", "b", "x", "y"}, mergedList.toArray());
 	}
 }
