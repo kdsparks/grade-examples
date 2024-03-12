a283bcfaf623c960f3fdbc1f8a1c046aa834afc0
kdsparks <156236500+kdsparks@users.noreply.github.com>
2024-03-11 21:42:25 -0700

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -5,20 +5,6 @@ interface StringChecker { boolean checkString(String s); }
 
 class ListExamples {
 
-	// Returns a new list that has all the elements of the input list for which
-	// the StringChecker returns true, and not the elements that return false, in
-  	// the same order they appeared in the input list;
-	static List<String> filter(List<String> list, StringChecker sc) {
-		List<String> result = new ArrayList<>();
-		for(String s: list) {
-			if(sc.checkString(s)) {
-				result.add(s);
-			}
-		}
-		return result;
-	}
-
-
 	// Takes two sorted list of strings (so "a" appears before "b" and so on),
 	// and return a new list that has all the strings in both list in sorted order.
 	static List<String> merge(List<String> list1, List<String> list2) {
