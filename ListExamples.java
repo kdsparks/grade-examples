f15575c9cb76068e209073216eb5fdec15d832dc
kdsparks <156236500+kdsparks@users.noreply.github.com>
2024-03-12 00:09:12 -0700

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -9,7 +9,7 @@ class ListExamples {
 	// and return a new list that has all the strings in both list in sorted order.
 	static void merge(List<String> list1, List<String> list2, List<String> mergedList) {
 		int index1 = 0, index2 = 0;
-		while(index1 < list1.size() || index2 < list2.size()) {
+		while(index1 < list1.size() && index2 < list2.size()) {
 			if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
 				mergedList.add(list1.get(index1));
 				index1 += 1;
