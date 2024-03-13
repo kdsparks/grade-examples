177fbea2f10893732a1b8626903caa337e9eb656
kdsparks <156236500+kdsparks@users.noreply.github.com>
2024-03-12 17:11:42 -0700

    Create Grade.java

---
+++ Grade.java
@@ -0,0 +1,17 @@
+public class Grade {
+	private int score;
+	private int weight;
+
+	public Grade(int score, int weight) {
+		this.score = score;
+		this.weight = weight;
+	}
+	
+	public int getScore() {
+		return this.score;
+	}
+
+	public int getWeight() {
+		return this.weight;
+	}
+}
