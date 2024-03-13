d03a56becb3ac7adf93bdee274dd42e32a486a6e
kdsparks <156236500+kdsparks@users.noreply.github.com>
2024-03-12 17:02:07 -0700

    Update and rename ListExamples.java to Grades.java

---
+++ Grades.java
@@ -0,0 +1,12 @@
+//import java.util.ArrayList;
+//import java.util.List;
+
+//interface StringChecker { boolean checkString(String s); }
+
+class Grades {
+
+	// 
+	public String findGrade(Grade[] scores) {
+		return null;
+	}
+}
