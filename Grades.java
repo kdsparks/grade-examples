59dcc2db2f5b842e83d1743f307b35a38a775c0f
kdsparks <156236500+kdsparks@users.noreply.github.com>
2024-03-12 17:26:38 -0700

    Update Grades.java

--- Grades.java
+++ Grades.java
@@ -1,12 +1,32 @@
-//import java.util.ArrayList;
-//import java.util.List;
 
-//interface StringChecker { boolean checkString(String s); }
-
-class Grades {
+public class Grades {
 
 	// 
 	public String findGrade(Grade[] scores) {
-		return null;
+		double grade = 0;
+		for(int i = 0; i < scores.length; i++) {
+			int score = scores[i].getScore();
+			int weight = scores[i].getWeight();
+			grade += score * ((double)weight / 100);
+		}
+
+		if(grade >= 90) {
+			return "A";
+		}
+		else if(grade >= 80) {
+			return "B";
+		}
+		else if(grade >= 70) {
+			return "C";
+		}
+		else if(grade >= 60) {
+			return "D";
+		}
+		else if(grade >= 0) {
+			return "F";
+		}
+		else {
+			return "invalid input";
+		}
 	}
 }
