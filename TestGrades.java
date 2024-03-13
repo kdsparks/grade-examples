ae9181b785191f1939f60f575a8f6a03151bb7a7
kdsparks <156236500+kdsparks@users.noreply.github.com>
2024-03-12 17:47:00 -0700

    Update TestGrades.java

--- TestGrades.java
+++ TestGrades.java
@@ -1,13 +1,19 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-import java.util.*;
 
 
 public class TestGrades {
 	@Test(timeout = 500)
-	public void testCalculateGrade() {
+	public void testCalculateGradeA() {
 		Grade[] scores = {new Grade(100, 50), new Grade(90, 30), new Grade(85, 20)};
 		String theGrade = Grades.findGrade(scores);
 		assertEquals(theGrade, "A");
+	}
+
+	@Test(timeout = 500)
+	public void testCalculateGradeC() {
+		Grade[] scores = {new Grade(100, 5), new Grade(90, 30), new Grade(80, 65)};
+		String theGrade = Grades.findGrade(scores);
+		assertEquals(theGrade, "C");
 	}
 }
