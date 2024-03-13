e3ac7915939ae11110a03980a294aea45918ec28
kdsparks <156236500+kdsparks@users.noreply.github.com>
2024-03-12 20:03:16 -0700

    Update TestGrades.java

--- TestGrades.java
+++ TestGrades.java
@@ -3,14 +3,14 @@ import org.junit.*;
 
 
 public class TestGrades {
-	//@Test(timeout = 500)
+	@Test(timeout = 500)
 	public void testCalculateGradeA() {
 		Grade[] scores = {new Grade(100, 50), new Grade(90, 30), new Grade(85, 20)};
 		String theGrade = Grades.findGrade(scores);
 		assertEquals("A", theGrade);
 	}
 
-	//@Test(timeout = 500)
+	@Test(timeout = 500)
 	public void testCalculateGradeC() {
 		Grade[] scores = {new Grade(100, 5), new Grade(0, 30), new Grade(100, 70)};
 		String theGrade = Grades.findGrade(scores);
